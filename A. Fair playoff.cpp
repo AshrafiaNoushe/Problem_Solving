8d60599c319db2085808c8fba54e7a9edcd35d53
Ashrafia Noushe <ashrafianoushe779@gmail.com>
2025-03-31 10:41:54 +0600

    313 A

--- A. Fair playoff.cpp
+++ A. Fair playoff.cpp
@@ -9,12 +9,12 @@ int main()
         for(int i=0;i<4;i++){
             cin>>s[i];
         }
-        int fir=0,lir=0,x,y;
-        fir = max(s[0],s[1]);
+        int first_two=0,last_two=0,x,y;
+        first_two = max(s[0],s[1]);
         x = min(s[0],s[1]);
         y = min(s[2],s[3]);
-        lir = max(s[2],s[3]);
-        if(fir>y && lir >x){
+        last_two = max(s[2],s[3]);
+        if(first_two>y && last_two >x){
             cout<<"YES"<<endl;
         }else{
             cout<<"NO"<<endl;
