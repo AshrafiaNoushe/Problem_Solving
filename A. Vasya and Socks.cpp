040464fc680354c9ea599d1d513762fa3c47380d
Ashrafia Noushe <ashrafianoushe779@gmail.com>
2025-04-04 14:22:34 +0600

    460 A

---
+++ A. Vasya and Socks.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n,m;
+    cin>>n>>m;
+    int days=0,socks=n;
+    while(socks>0){
+        days++;
+        socks--;
+        if(days%m==0){
+            socks++;
+        }
+    }
+    cout<<days<<endl;
+    
+}
