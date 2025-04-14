86a79337052d7a41343481eeefa5c95be0db265d
Ashrafia Noushe <ashrafianoushe779@gmail.com>
2025-04-14 10:56:37 +0600

    2094 C

---
+++ C. Brr Brrr Patapim.cpp
@@ -0,0 +1,43 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t;
+    cin>>t;
+    while(t--){
+        int n;cin>>n;
+        long long a[n+10][n+10];
+        map<int,int>mp;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n;j++){
+                cin>>a[i][j];
+                mp[i+j]=a[i][j];
+            }
+        }
+        vector<int>v;
+        set<int>st;
+        for(int i=1;i<=2*n;i++){
+            if(mp[i]!=0){
+                st.insert(mp[i]);
+            }
+        }
+        for(int i=1;i<=2*n;i++){
+            if(st.find(i)==st.end()){
+                v.push_back(i);
+            }
+        }
+        for(int i=1;i<=2*n;i++){
+            if(mp[i]==0){
+                cout<<v.back()<<" ";//amr kace map er i position e value nai tai v theke value anci print dci abar dlt dci
+                v.pop_back();//son 2n position fill kora lagbe so ami missing v theke last value ta anci mp e
+            }else{
+                cout<<mp[i]<<" ";
+            }
+        }
+        cout<<endl;
+        
+    }
+    return 0;
+}
