063fdeba081df0a6c5815d03327067ba8642ebfa
Ashrafia Noushe <ashrafianoushe779@gmail.com>
2025-04-21 15:23:33 +0600

    800 rating

---
+++ B. Who's Opposite.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t;
+    cin>>t;
+    while(t--){
+        int a,b,c;
+        cin>>a>>b>>c;
+        if(abs(a-b)>c){
+            cout<<"-1"<<endl;
+            return 0;
+        }else{
+            cout<<abs(a-b)+c<<endl;
+        }
+    }
+    return 0;
+}
